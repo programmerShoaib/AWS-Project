f33cb038a6ffb71a361e59492274dfa61bdc800d
programmerShoaib <link2shoaib.2002@gmail.com>
2026-01-03 11:18:41 +0500

    deletion

--- resources/views/users/edit.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<form method="POST" action="/update/{{ $user->id }}">@csrf
-<input name="name" value="{{ $user->name }}">
-<input name="email" value="{{ $user->email }}">
-<button>Update</button>
-</form>
-@endsection
--- resources/views/users/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-@section('content')
-<a href="/create" class="bg-indigo-600 text-white px-4 py-2">Add User</a>
-@foreach($users as $u)
-<p>{{ $u->name }} {{ $u->email }} 
-<a href="/edit/{{ $u->id }}">Edit</a> 
-<a href="/delete/{{ $u->id }}">Delete</a></p>
-@endforeach
-@endsection
